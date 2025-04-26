dfe353bc049f4a239e6f25964526c4eaf42b983a
Josh w. <lokiofasgard04@gmail.com>
2025-04-25 20:14:41 -0400

    Made partner classes n shiz

--- LanguagePairingProgram/app/src/main/java/com/onturaa/languagepairingprogram/model/PartnerRepository.kt
+++ LanguagePairingProgram/app/src/main/java/com/onturaa/languagepairingprogram/model/PartnerRepository.kt
@@ -1,4 +1,15 @@
 package com.onturaa.languagepairingprogram.model
 
+import java.util.Dictionary
+
 class PartnerRepository {
+    enum class Languages{
+        Japanese,
+        Patwa,
+        English,
+        Twi
+    }
+    data class Partner(val n: String, val netId: String, val year: Int, val languages: Dictionary<Languages, Int>) {
+
+    }
 }
--- LanguagePairingProgram/app/src/main/java/com/onturaa/languagepairingprogram/ui/components/PartnerCard.kt
+++ LanguagePairingProgram/app/src/main/java/com/onturaa/languagepairingprogram/ui/components/PartnerCard.kt
@@ -1,2 +1,18 @@
 package com.onturaa.languagepairingprogram.ui.components
 
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.onturaa.languagepairingprogram.model.PartnerRepository
+import java.util.Dictionary
+
+@Composable
+fun PartnerCard (partner: PartnerRepository.Partner, lang: PartnerRepository.Languages ) {
+    Card {
+
+    }
+}
+@Composable
+@Preview
+fun PartnerCardPreview(){}
+
