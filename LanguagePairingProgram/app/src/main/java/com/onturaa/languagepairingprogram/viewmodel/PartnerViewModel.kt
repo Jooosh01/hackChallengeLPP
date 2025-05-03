5d48fc9d9fab91773c6349afe1ad53a64c8ab7ce
Josh w. <lokiofasgard04@gmail.com>
2025-05-02 20:38:40 -0400

    suffering

--- LanguagePairingProgram/app/src/main/java/com/onturaa/languagepairingprogram/viewmodel/PartnerViewModel.kt
+++ LanguagePairingProgram/app/src/main/java/com/onturaa/languagepairingprogram/viewmodel/PartnerViewModel.kt
@@ -1,4 +1,33 @@
 package com.onturaa.languagepairingprogram.viewmodel
 
-class PartnerViewModel {
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.onturaa.languagepairingprogram.model.PartnerRepository
+import com.onturaa.languagepairingprogram.retrofit.ApiService
+import com.onturaa.languagepairingprogram.retrofit.RetrofitInstance
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class PartnerViewModel @Inject constructor(
+private val retrofitInstance: RetrofitInstance,
+) : ViewModel(){
+    class PartnerViewModel(
+        private val apiService: ApiService
+    ) : ViewModel() {
+
+        var users by mutableStateOf<List<PartnerRepository.Partner>>(emptyList())
+            private set
+
+
+        fun getMatches(userId: Int, token: String) {
+            viewModelScope.launch {
+
+                users = apiService.get_matches(userId)
+
+            }
+        }
+    }
 }
